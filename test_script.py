d8ca841db8862eb5085e801137b3c1aa0e6edc7f
Alex Lee <alex.m.lee10@gmail.com>
2025-07-24 19:37:40 -0400

    update style

--- test_script.py
+++ test_script.py
@@ -7,7 +7,7 @@ motorkit = MotorKit(i2c=board.I2C())
 
 print("starting stepper motor")
 for i in range(200):
-    motorkit.stepper2.onestep()
+    motorkit.stepper2.onestep(style=stepper.DOUBLE)
 
 time.sleep(5)
 
