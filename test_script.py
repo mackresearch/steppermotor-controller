b227e346bda6160ded348d2ad51b92e75e05f366
Alex Lee <alex.m.lee10@gmail.com>
2025-07-24 12:34:33 -0400

    add print statement in between each step

--- test_script.py
+++ test_script.py
@@ -8,6 +8,7 @@ motorkit = MotorKit(i2c=board.I2C())
 print("starting stepper motor")
 
 for i in range (200):
+    print(i)
     motorkit.stepper2.onestep(style=stepper.SINGLE)
     time.sleep(.05)
 
