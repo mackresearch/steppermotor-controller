070f94ba6a05603315e06dcdbf2b411b3ff0d4d9
Alex Lee <alex.m.lee10@gmail.com>
2025-07-24 19:26:44 -0400

    more stuff

--- test_script.py
+++ test_script.py
@@ -4,10 +4,20 @@ import board
 import time
 
 motorkit = MotorKit(i2c=board.I2C())
-
 print("starting stepper motor")
+for i in range(300):
+    motorkit.stepper2.onestep()
+
+time.sleep(2)
+
 for i in range (475):
-    motorkit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
+    motorkit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
     time.sleep(.01)
+
+time.sleep(2)
+
+for i in range(250):
+    motorkit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
+
 
 motorkit.stepper2.release()
