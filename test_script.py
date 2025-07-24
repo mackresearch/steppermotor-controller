38c062e65350b6eab3c48efb28f85558bbcd1ed3
Alex Lee <alex.m.lee10@gmail.com>
2025-07-24 19:17:15 -0400

    update direction to backward

--- test_script.py
+++ test_script.py
@@ -7,7 +7,7 @@ motorkit = MotorKit(i2c=board.I2C())
 
 print("starting stepper motor")
 for i in range (475):
-    motorkit.stepper2.onestep(style=stepper.SINGLE)
+    motorkit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
     time.sleep(.01)
 
 motorkit.stepper2.release()
